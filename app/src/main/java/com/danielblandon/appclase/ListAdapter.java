6e0171ea789fe39c71586720bfa1a1b393c423ca
Your name here <danielblandon_01@hotmail.com>
2023-10-02 19:30:11 -0500

    Projecto ListAdapter

---
+++ app/src/main/java/com/danielblandon/appclase/ListAdapter.java
@@ -0,0 +1,47 @@
+package com.danielblandon.appclase;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public class ListAdapter extends BaseAdapter {
+    // Context es para saber el contexto donde estoy
+    Context contexto ;
+
+    String[] nombres,mensajes,horaUltMen,numTel,pais;
+    int idImagen;
+    LayoutInflater inflater;
+    public ListAdapter(Context contexto, String[] nombres, String[] mensajes, String[] horaUltMen, String[] numTel, String[] pais, int idImagen) {
+        this.contexto = contexto;
+        this.nombres = nombres;
+        this.mensajes = mensajes;
+        this.horaUltMen = horaUltMen;
+        this.numTel = numTel;
+        this.pais = pais;
+        this.idImagen = idImagen;
+        inflater = LayoutInflater.from(contexto);
+
+    }
+    //recorremos el arreglo para saber cuantos datos tenemos
+    @Override
+    public int getCount(){return nombres.length;}
+
+    @Override
+    public Object getItem(int i){return null;}
+
+    @Override
+    public long getItemId(int i){return 0;}
+
+    @Override
+    //obtener la vista(parte grafica)
+    public View getView(int i, View vista, ViewGroup viewGroup){
+        vista = inflater.inflate(R.layout.list_item,null);
+
+        ImageView  fotoPerfil =vista.findViewById(R.id.)
+        return vista;}
+
+
+}
---
+++ app/src/main/java/com/danielblandon/appclase/MainActivity.java
@@ -0,0 +1,14 @@
+package com.danielblandon.appclase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
